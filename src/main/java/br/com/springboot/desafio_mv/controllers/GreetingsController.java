3826e51f7b6a811789cf7b23c846c4b7fe9054f9
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-16 23:40:02 -0300

    Criando END-POINT de listar todos usuários

--- src/main/java/br/com/springboot/desafio_mv/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/desafio_mv/controllers/GreetingsController.java
@@ -1,13 +1,19 @@
 package br.com.springboot.desafio_mv.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.springboot.desafio_mv.model.Usuario;
 import br.com.springboot.desafio_mv.repository.CafeRepository;
 import br.com.springboot.desafio_mv.repository.UsuarioRepository;
 
@@ -18,10 +24,10 @@ import br.com.springboot.desafio_mv.repository.UsuarioRepository;
 @RestController
 public class GreetingsController {
 
-	@Autowired //Injeção de dependência
+	@Autowired // Injeção de dependência
 	private UsuarioRepository usuarioRepository;
-	
-	@Autowired //Injeção de dependência
+
+	@Autowired // Injeção de dependência
 	private CafeRepository cafeRepository;
 
 	/**
@@ -34,4 +40,15 @@ public class GreetingsController {
 	public String greetingText(@PathVariable String name) {
 		return "Hello " + name + "!";
 	}
+
+	// Método para listar todos os usuários do banco de dados
+	@GetMapping(value = "/listartodos")
+	@ResponseBody // Retorna dados para o corpo da resposta
+	public ResponseEntity<List<Usuario>> listarUsuarios() {	
+
+		List<Usuario> usuarios = usuarioRepository.findAll(); // Execulta a consulta no banco de dados
+
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); // Retorna a lista em JSON
+	}
+
 }
