faf874b7a52e1854cb76e93cadf8dfb96f3cbe7c
uluizeduardo <luiz.analistadesistemas@gmail.com>
2021-10-17 11:36:35 -0300

    Criando END-POINT para DELETE do banco

--- src/main/java/br/com/springboot/desafio_mv/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/desafio_mv/controllers/GreetingsController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,5 +59,13 @@ public class GreetingsController {
 	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
 		Usuario user = usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario>(user, HttpStatus.ACCEPTED);
+	}
+	
+	// Método para deletar usuários do banco de dados
+	@DeleteMapping(value = "delete")
+	@ResponseBody
+	public ResponseEntity<String> delet(@RequestParam Long idUser){
+		usuarioRepository.deleteById(idUser);
+		return new ResponseEntity<String>("Usuário deletado com sucesso!", HttpStatus.OK);
 	}
 }
